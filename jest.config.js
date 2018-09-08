3ea88bba84762c34235bcaa037643f2f9bea0395
Joel Marques <joel.marques@keyrus.com.br>
2018-09-08 14:46:53 -0300

    Criando testes unitarios

---
+++ jest.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    verbose: false,
+    collectCoverage: true,
+    collectCoverageFrom: [
+      'app/**/*.{js}',
+      '!**/tmp/**',
+      '!**/coverage/**',
+      '!**/node_modules/**',
+      '!**/config/**',
+      '!**/public/**',
+    ],
+    testPathIgnorePatterns: [
+      '/tmp/',
+      '/mock/',
+      '/coverage/',
+    ],
+    coverageDirectory: 'coverage',
+    coverageReporters: ['lcov', 'text', 'html', 'cobertura'],
+    coverageThreshold: {
+      global: {
+        branches: 100,
+        functions: 100,
+        lines: 100,
+        statements: 100,
+      },
+    },
+  };
