2e318db740b8cfdba753fe0f13a330ad0c7f9b51
Janderson Siva <janderson.tic@gmail.com>
2018-10-06 16:51:56 -0300

    Exemplo de ESC Class. Classes em JavaScript

--- src/index.js
+++ src/index.js
@@ -1,14 +1,42 @@
-import { getNome, getIdade, getEndereco } from './util';
+import Automovel from './componentes/Automovel';
 
-export const getUsuario = () => {
-  const node = document.createElement('li');
-  const textnode = document.createTextNode('Fulano, 28, Avenida Industrial 1580 AP 17 Torre C');
-  node.appendChild(textnode);
-  document.getElementById('mylist').appendChild(node);
+export const criarCarro = (automovel) => {
+  const marca = document.createElement('li');
+  const textnode = document.createTextNode(`Marcas: ${automovel.verModelo()} e ${automovel.verModelo()}`);
+  marca.appendChild(textnode);
+
+  document.getElementById('mylist').appendChild(marca);
 };
 
-window.getUsuario = getUsuario;
+export const imprirDados = (automovel) => {
+  const textAutoLigado = `${automovel.verModelo()} está ligado? ${(automovel.estaLigado() ? 'Sim' : 'Não')}`;
+
+  const elementoLi = document.createElement('li');
+  const texto = document.createTextNode(textAutoLigado);
+  elementoLi.appendChild(texto);
+  document.getElementById('motorLigado').appendChild(elementoLi);
+};
+
+export const ligarCarro = (automovel, chave) => {
+  automovel.ligarMotor(chave);
+};
+
+const modelos = ['Uno', 'HB20', 'Gol', 'Lifan', 'Civic'];
+
+modelos.forEach((modelo, indice) => {
+  const automovel = new Automovel(modelo, 'v2');
+  automovel.ligarMotor({
+    segredo: `v${indice}`,
+  });
+  imprirDados(automovel);
+});
+
+window.automovel = {
+  criarCarro,
+  ligarCarro,
+};
 
 export default {
-  getUsuario,
+  criarCarro,
+  ligarCarro,
 };
