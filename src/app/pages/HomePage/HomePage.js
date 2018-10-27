1ef7c946a21082af85d2b492eeae98fd4a5befc3
jandersonTIC <janderson.tic@gmail.com>
2018-10-27 19:00:30 -0300

    Uso do componente List na HomePage com dados mockados em substituição do uso explícito de Cards

--- src/app/pages/HomePage/HomePage.js
+++ src/app/pages/HomePage/HomePage.js
@@ -5,7 +5,9 @@ import { Helmet } from 'react-helmet';
 
 import Grid from '@material-ui/core/Grid';
 import { withStyles } from '@material-ui/core/styles';
-import CardPage from './CardPage';
+// import CardPage from '../../components/CardPage';
+// import CardPage from '@/components/CardPage';
+import List from '../../components/List';
 
 const styles = theme => ({
   root: {
@@ -31,6 +33,45 @@ const styles = theme => ({
   },
 });
 
+const produtos = [
+  {
+    id: 1,
+    title: 'Item 1',
+    description: 'Descrição do Item 1',
+    image: 'imagem-1.jpg',
+  },
+  {
+    id: 2,
+    title: 'Item 2',
+    description: 'Descrição do Item 2',
+    image: 'imagem-2.jpg',
+  },
+  {
+    id: 3,
+    title: 'Item 3',
+    description: 'Descrição do Item 3',
+    image: 'imagem-3.jpg',
+  },
+  {
+    id: 4,
+    title: 'Item 4',
+    description: 'Descrição do Item 4',
+    image: 'imagem-4.jpg',
+  },
+  {
+    id: 5,
+    title: 'Item 5',
+    description: 'Descrição do Item 5',
+    image: 'imagem-5.jpg',
+  },
+  {
+    id: 6,
+    title: 'Item 6',
+    description: 'Descrição do Item 6',
+    image: 'imagem-6.jpg',
+  },
+];
+
 class HomePageComponent extends React.Component {
   render() {
     const { classes } = this.props;
@@ -41,16 +82,7 @@ class HomePageComponent extends React.Component {
         <Grid container className={classes.root}>
           <Grid item xs={12} md={12} className={classes.box}>
             <div>Página Inicial Trocapp</div>
-            <CardPage
-              image="sofa.jpg"
-              title="Sofá Retrátil"
-              description="Lorem Ipsum lorem lorem"
-            />
-            <CardPage
-              image="sofa.jpg"
-              title="Sofá"
-              description="Lorem Ipsum lorem lorem"
-            />
+            <List produtos={produtos} />
           </Grid>
         </Grid>
       </React.Fragment>
